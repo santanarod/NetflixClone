795a19f1015cd106edbc0d8592a8c22dec1a66b3
Allan Santana <santana.rod15@gmail.com>
2022-03-24 19:29:20 -0300

    change api_key

--- NetflixClone/Resources/Constants.swift
+++ NetflixClone/Resources/Constants.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct Constants {
-    static let API_KEY = "da1ac2f875782ec49de0a8c252e05fbf"
+    static let API_KEY = "697d439ac993538da4e3e60b54e762cd"
     static let baseURL = "https://api.themoviedb.org"
     
 }
